1f3957d4cbc4acedb828dedcce307c8f8d36936c
swap-3223 <waghswapnil@gmail.com>
2024-07-10 16:18:10 +0530

    blog app

---
+++ Code-Backup/Mega-Blog/src/components/index.js
@@ -0,0 +1,7 @@
+import Header from "./Headers/Header";
+import Footer from "./Footers/Footer";
+import Container from './containers/Container';
+import Logo from "./Logos"
+import LogoutBtn from "./Headers/LogoutBtn";
+
+export {Footer,Header,Container,Logo,LogoutBtn};
