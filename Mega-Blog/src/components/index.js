f60cd7cb3ba58768d6f270438d18011b402c5eff
swap-3223 <waghswapnil@gmail.com>
2024-07-11 23:33:33 +0530

    post-form created

--- Mega-Blog/src/components/index.js
+++ Mega-Blog/src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footers/Footer";
 import Container from './containers/Container';
 import Logo from "./Logos"
 import LogoutBtn from "./Headers/LogoutBtn";
+import RTE from "./RTE";
 
-export {Footer,Header,Container,Logo,LogoutBtn};
+export {Footer,Header,Container,Logo,LogoutBtn,RTE};
