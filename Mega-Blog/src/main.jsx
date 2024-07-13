5ccd308645f863583df2b540e484d995c3813728
swap-3223 <waghswapnil@gmail.com>
2024-07-13 14:28:07 +0530

    in process

--- Mega-Blog/src/main.jsx
+++ Mega-Blog/src/main.jsx
@@ -1,8 +1,3 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
-// import {Provider} from "react-redux"
 import Store from "./Storage/Store.jsx"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
